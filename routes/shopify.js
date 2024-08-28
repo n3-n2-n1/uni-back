7893301347c77d37676f4c45bcbd05570b872b7a
thomgome <thomgome@emeal.nttdata.com>
2024-08-28 11:38:35 -0300

    add for fixes

--- routes/shopify.js
+++ routes/shopify.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const axios = require('axios');
-const express = require('express');
+
 const shopifyRouter = () => {
-const router = express.Router();
+  const router = express.Router();
+
+  // Configuración de Axios con autenticación básica
+  const shopifyAxios = axios.create({
+    baseURL: `https://${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2024-01`,
+    auth: {
+      username: process.env.SHOPIFY_API_KEY,
+      password: process.env.SHOPIFY_API_PASSWORD
+    }
+  });
 
   router.get('/shopify/products', async (req, res) => {
     try {
-      const shopifyResponse = await axios.get(`https://${process.env.SHOPIFY_API_KEY}:${process.env.SHOPIFY_API_PASSWORD}@${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2024-01/collections/478674190640/products.json`);
-
+      const shopifyResponse = await shopifyAxios.get('/collections/478674190640/products.json');
       res.json(shopifyResponse.data.products);  // Envía los productos al frontend
     } catch (error) {
       console.error('Error fetching products from Shopify:', error.message);
       res.status(500).json({ error: 'Error fetching products from Shopify' });
     }
   });
-}
+
+  return router;  // No olvides retornar el router
+};
 
 module.exports = shopifyRouter;
