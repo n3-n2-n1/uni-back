420f272b4ba31278237d91f6b0983a8625d6d833
thomgome <thomgome@emeal.nttdata.com>
2024-08-28 12:06:56 -0300

    Shopify conn

--- routes/shopify.js
+++ routes/shopify.js
@@ -4,26 +4,55 @@ const axios = require('axios');
 const shopifyRouter = () => {
   const router = express.Router();
 
-  // Configuración de Axios con autenticación básica
+  // Configuración de Axios con el token de acceso para Storefront API
   const shopifyAxios = axios.create({
-    baseURL: `https://${process.env.SHOPIFY_STORE_NAME}.myshopify.com/admin/api/2024-01`,
-    auth: {
-      username: process.env.SHOPIFY_API_KEY,
-      password: process.env.SHOPIFY_API_PASSWORD
+    baseURL: `https://012078-0f.myshopify.com/api/unstable/graphql.json`,
+    headers: {
+      'X-Shopify-Storefront-Access-Token': '124cc182b560cc1a0aaa3d2993474093', // Asegúrate de que esta variable esté configurada correctamente
+      'Content-Type': 'application/json'
     }
   });
 
   router.get('/shopify/products', async (req, res) => {
+    const query = `
+      {
+        collection(id: "gid://shopify/Collection/478674190640") {
+          products(first: 10) {
+            edges {
+              node {
+                id
+                title
+                description
+                priceRange {
+                  minVariantPrice {
+                    amount
+                  }
+                }
+                images(first: 1) {
+                  edges {
+                    node {
+                      src
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
     try {
-      const shopifyResponse = await shopifyAxios.get('/collections/478674190640/products.json');
-      res.json(shopifyResponse.data.products);  // Envía los productos al frontend
+      const shopifyResponse = await shopifyAxios.post('', { query });
+      const products = shopifyResponse.data.data.collection.products.edges.map(edge => edge.node);
+      res.json(products);  // Envía los productos al frontend
     } catch (error) {
       console.error('Error fetching products from Shopify:', error.message);
       res.status(500).json({ error: 'Error fetching products from Shopify' });
     }
   });
 
-  return router;  // No olvides retornar el router
+  return router; // No olvides retornar el router
 };
 
 module.exports = shopifyRouter;
